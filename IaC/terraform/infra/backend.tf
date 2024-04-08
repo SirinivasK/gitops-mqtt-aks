66dc43f1fcff473d343ed99c46ba947b442cd684
SrinivasK <srinivas.kondepudi@sebrands.com>
2024-04-08 22:49:25 +0200

    final changes

--- IaC/terraform/infra/backend.tf
+++ IaC/terraform/infra/backend.tf
@@ -1,6 +1,6 @@
 # terraform {
 #   backend "azurerm" {
-#     storage_account_name = "sebterraformprodsa"
+#     storage_account_name = "sriniterraformprodsa"
 #     container_name       = "statefiles"
 #     key                  = "gitops-mqtt-aks/state." #this is the blob name; terraform appends env:<workspace> to the end of the name.
 #     use_azuread_auth     = true
